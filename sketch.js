9436ce5c1a5b681fc04da8cc9ee03737d1d90794
zoereillat-lang <zoereillat@gmail.com>
2026-04-08 08:40:13 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,1 +1,17 @@
+window.addEventListener("scroll", function () {
 
+  let panels = document.querySelectorAll(".panel");
+
+  panels.forEach(function(panel) {
+
+    let position = panel.getBoundingClientRect().top;
+    let screenPosition = window.innerHeight / 1.3;
+
+    if (position < screenPosition) {
+      panel.style.opacity = 1;
+      panel.style.transform = "translateY(0)";
+    }
+
+  });
+
+});
